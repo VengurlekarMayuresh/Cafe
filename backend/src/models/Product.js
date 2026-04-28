8b0003015a96b10fd59ba508815fc139bcd83f4a
Mayuresh Vengurlekar <onlinemayuresh29@gmail.com>
2026-04-28 21:25:10 +0530

    product.js

---
+++ backend/src/models/Product.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Product = sequelize.define('Product', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    is_available: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  }, {
+    tableName: 'products',
+    timestamps: true,
+    underscored: true,
+    indexes: [
+      { fields: ['is_available'] },
+    ],
+  });
+
+  Product.associate = (models) => {
+    Product.hasMany(models.OrderItem, { foreignKey: 'product_id', as: 'orderItems' });
+  };
+
+  return Product;
+};
