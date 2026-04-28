cc402e549e34a0b47754f26b33e3d01271eaae88
Mayuresh Vengurlekar <onlinemayuresh29@gmail.com>
2026-04-28 21:24:51 +0530

    notificastion.js

---
+++ backend/src/models/Notification.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Notification = sequelize.define('Notification', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    is_read: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }, {
+    tableName: 'notifications',
+    timestamps: true,
+    underscored: true,
+  });
+
+  Notification.associate = (models) => {
+    Notification.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+  };
+
+  return Notification;
+};
