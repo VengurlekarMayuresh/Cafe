5afdb9c0aacef691613bb9d783b450e15c53afa7
Mayuresh Vengurlekar <onlinemayuresh29@gmail.com>
2026-04-28 21:22:15 +0530

    SQL

---
+++ backend/setup.sql
@@ -0,0 +1,6 @@
+-- Run this SQL to set up the database
+CREATE DATABASE society_order;
+
+-- Connect to society_order and run:
+-- The tables will be auto-created by Sequelize when you start the server
+-- Or run: node src/scripts/seed.js
